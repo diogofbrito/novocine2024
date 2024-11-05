f8da9e5b63c019859036c9712f28067b996c8464
Derek Nguyen <dikanguok@gmail.com>
2024-11-05 00:47:49 +0000

    feat: search

--- src/components/ArquivoComponentes/FilterSearch.jsx
+++ src/components/ArquivoComponentes/FilterSearch.jsx
@@ -1,113 +1,247 @@
-import React, { useState, useEffect } from 'react';
-import { Search, ChevronDown, ChevronUp } from 'lucide-react';
+import React, { useState, useEffect, useRef } from "react";
+import { Search, ChevronDown, ChevronUp } from "lucide-react";
+import sanityClient from "../../SanityClient";
 
-export function FilterSearch({ films, onSearchChange, onToggleView, isListView  }) {
-	const [searchTerm, setSearchTerm] = useState('');
-	const [selectedYear, setSelectedYear] = useState('');
-	const [selectedCountry, setSelectedCountry] = useState('');
-	const [isYearOpen, setIsYearOpen] = useState(false);
-	const [isCountryOpen, setIsCountryOpen] = useState(false);
-	const [years, setYears] = useState([]);
-	const [countries, setCountries] = useState([]);
+const handleClickOutside = (el) => (e) => {
+    return e.target.contains(el);
+};
 
-	useEffect(() => {
-		const uniqueYears = Array.from(new Set(films.map(film => film.ano))).sort((a, b) => b - a);
-		const uniqueCountries = Array.from(new Set(films.map(film => film.pais))).sort();
+export function FilterSearch({
+    searchTerm,
+    setSearchTerm,
+    selectedYear,
+    setSelectedYear,
+    selectedCountry,
+    setSelectedCountry,
+    films,
+    onToggleView,
+    isListView,
+}) {
+    const [isYearOpen, setIsYearOpen] = useState(false);
+    const [isCountryOpen, setIsCountryOpen] = useState(false);
+    const [years, setYears] = useState([]);
+    const [countries, setCountries] = useState([]);
+    const [fiYears, setFiYears] = useState([]);
+    const [fiCountries, setFiCountries] = useState([]);
 
-		setYears(uniqueYears);
-		setCountries(uniqueCountries);
-	}, [films]);
+    useEffect(() => {
+        const uniqueYears = Array.from(
+            new Set(films.map((film) => film.ano))
+        ).sort((a, b) => b - a);
+        const uniqueCountries = Array.from(
+            new Set(films.map((film) => film.pais))
+        ).sort();
 
-	const handleSearchChange = e => {
-		const term = e.target.value;
-		setSearchTerm(term);
-		onSearchChange({ searchTerm: term, selectedYear, selectedCountry });
-	};
+        setFiYears(uniqueYears);
+        setFiCountries(uniqueCountries);
 
-	const handleYearClick = year => {
-		const newYear = year === selectedYear ? '' : year;
-		setSelectedYear(newYear);
-		setIsYearOpen(false);
-		onSearchChange({ searchTerm, selectedYear: newYear, selectedCountry });
-	};
+        const getMetaData = () =>
+            sanityClient.fetch(`
+				{
+					"years": array::unique(*[
+						  _type == "filme" 
+						  && !(_id in path("drafts.**"))
+					].ano),
+					"countries": array::unique(*[
+						  _type == "filme" 
+						  && !(_id in path("drafts.**"))
+					].pais),
+				}
+			`);
 
-	const handleCountryClick = country => {
-		const newCountry = country === selectedCountry ? '' : country;
-		setSelectedCountry(newCountry);
-		setIsCountryOpen(false);
-		onSearchChange({ searchTerm, selectedYear, selectedCountry: newCountry });
-	};
+        getMetaData().then(({ years, countries }) => {
+            if (!years || !countries) {
+                throw new Error("No years, no countries list");
+            }
+            setYears(years.sort((a, b) => b - a));
+            setCountries(countries.sort());
+        });
+    }, [films]);
 
-	return (
-		<div className='z-50 flex  w-full justify-between items-center '>
-			<div className='flex gap-4 '>
-				<div className='flex border text-xl rounded-full px-3 py-1 items-center w-[400px] filter'>
-					<input
-						type='text'
-						placeholder='Procurar'
-						className='flex-grow border-none bg-transparent mr-2 outline-none text-base leading-none input-placeholder'
-						value={searchTerm}
-						onChange={handleSearchChange}
-					/>
-					<Search size={20} />
-				</div>
+    const handleSearchChange = (e) => {
+        const term = e.target.value;
+        setSearchTerm(term);
+    };
 
-				<div className='relative'>
-					<button
-						className='border rounded-full  py-1 px-3 flex items-center w-[200px] justify-between hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] filter'
-						onClick={() => setIsYearOpen(!isYearOpen)}
-					>
-						{selectedYear || 'Anos'} {isYearOpen ? <ChevronUp size={20} /> : <ChevronDown size={20} />}
-					</button>
+    const handleYearClick = (year) => {
+        const newYear = year === selectedYear ? "" : year;
+        setSelectedYear(newYear);
+        setIsYearOpen(false);
+    };
 
-					{isYearOpen && (
-						<ul className='list-none  mt-2 rounded-3xl border max-h-48 overflow-y-auto absolute w-full z-10 '>
-							<li
-								onClick={() => handleYearClick('')}
-								className='
-						 px-4 py-1 hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] cursor-pointer'
-							>
-								Todos os anos
-							</li>
-							{years.map(year => (
-								<li key={year} onClick={() => handleYearClick(year)} className='py-1 px-4 hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] cursor-pointer'>
-									{year}
-								</li>
-							))}
-						</ul>
-					)}
-				</div>
+    const handleYearClickReset = (year) => {
+        handleYearClick(year);
+        setSelectedCountry("");
+        setSearchTerm("");
+    };
 
-				<div className='relative'>
-					<button
-						className='border rounded-full py-1 px-3 flex items-center w-[200px] justify-between hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] filter'
-						onClick={() => setIsCountryOpen(!isCountryOpen)}
-					>
-						{selectedCountry || 'Países'} {isCountryOpen ? <ChevronUp size={20} /> : <ChevronDown size={20} />}
-					</button>
+    const handleCountryClick = (country) => {
+        const newCountry = country === selectedCountry ? "" : country;
+        setSelectedCountry(newCountry);
+        setIsCountryOpen(false);
+    };
 
-					{isCountryOpen && (
-						<ul className='list-none pt-0 mt-2 rounded-3xl  max-h-48 overflow-y-auto absolute w-full z-10 border '>
-							<li onClick={() => handleCountryClick('')} className='py-1 px-4 hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] cursor-pointer'>
-								Todos os países
-							</li>
-							{countries.map(country => (
-								<li key={country} onClick={() => handleCountryClick(country)} className='py-1 px-4 hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] cursor-pointer'>
-									{country}
-								</li>
-							))}
-						</ul>
-					)}
-				</div>
+    const handleCountryClickReset = (year) => {
+        handleCountryClick(year);
+        setSelectedYear("");
+        setSearchTerm("");
+    };
 
-				<div className='flex items-center '>
-					<p>{films.length} Filmes</p>
-				</div>
-			</div>
+    const yearMenuRef = useRef(null);
+    const countryMenuRef = useRef(null);
+    useEffect(() => {
+        if (!window) return;
+        document.addEventListener(
+            "click",
+            handleClickOutside(yearMenuRef.current)
+        );
+        document.addEventListener(
+            "click",
+            handleClickOutside(countryMenuRef.current)
+        );
 
-			<button onClick={onToggleView} className='border rounded-full px-3 py-1 items-center transition-all duration-300 ease-in-out'>
-				{isListView ? 'Galeria' : 'Lista'}
-			</button>
-		</div>
-	);
+        return () => {
+            document.removeEventListener(
+                "click",
+                handleClickOutside(yearMenuRef.current)
+            );
+            document.removeEventListener(
+                "click",
+                handleClickOutside(countryMenuRef.current)
+            );
+        };
+    }, [yearMenuRef.current, countryMenuRef.current]);
+
+    return (
+        <div className="z-50 flex  w-full justify-between items-center ">
+            <div className="flex gap-4 ">
+                <div className="flex border text-xl rounded-full px-3 py-1 items-center w-[400px] filter">
+                    <input
+                        type="text"
+                        placeholder="Procurar"
+                        className="flex-grow border-none bg-transparent mr-2 outline-none text-base leading-none input-placeholder"
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                    />
+                    <Search size={20} />
+                </div>
+
+                <div className="relative">
+                    <button
+                        className="border rounded-full  py-1 px-3 flex items-center w-[200px] justify-between hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] filter"
+                        onClick={() => setIsYearOpen(!isYearOpen)}
+                    >
+                        {selectedYear || "Anos"}{" "}
+                        {isYearOpen ? (
+                            <ChevronUp size={20} />
+                        ) : (
+                            <ChevronDown size={20} />
+                        )}
+                    </button>
+
+                    {isYearOpen && (
+                        <ul
+                            ref={yearMenuRef}
+                            className="list-none  mt-2 rounded-3xl border max-h-48 overflow-y-auto absolute w-full z-10 "
+                        >
+                            <li
+                                onClick={() => handleYearClick("")}
+                                className="
+						 px-4 py-1 hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] cursor-pointer"
+                            >
+                                Todos os anos
+                            </li>
+                            {fiYears.map((year) => (
+                                <li
+                                    key={year}
+                                    onClick={() => handleYearClick(year)}
+                                    className="py-1 px-4 hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] cursor-pointer"
+                                >
+                                    {year}
+                                </li>
+                            ))}
+                            <hr />
+                            {years
+                                .filter((year) => !fiYears.includes(year))
+                                .map((year) => (
+                                    <li
+                                        key={year}
+                                        onClick={() =>
+                                            handleYearClickReset(year)
+                                        }
+                                        className="py-1 px-4 hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] cursor-pointer opacity-50"
+                                    >
+                                        {year}
+                                    </li>
+                                ))}
+                        </ul>
+                    )}
+                </div>
+
+                <div className="relative">
+                    <button
+                        className="border rounded-full py-1 px-3 flex items-center w-[200px] justify-between hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] filter"
+                        onClick={() => setIsCountryOpen(!isCountryOpen)}
+                    >
+                        {selectedCountry || "Países"}{" "}
+                        {isCountryOpen ? (
+                            <ChevronUp size={20} />
+                        ) : (
+                            <ChevronDown size={20} />
+                        )}
+                    </button>
+
+                    {isCountryOpen && (
+                        <ul
+                            ref={countryMenuRef}
+                            className="list-none pt-0 mt-2 rounded-3xl  max-h-48 overflow-y-auto absolute w-full z-10 border "
+                        >
+                            <li
+                                onClick={() => handleCountryClick("")}
+                                className="py-1 px-4 hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] cursor-pointer"
+                            >
+                                Todos os países
+                            </li>
+                            {fiCountries.map((country) => (
+                                <li
+                                    key={country}
+                                    onClick={() => handleCountryClick(country)}
+                                    className="py-1 px-4 hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] cursor-pointer"
+                                >
+                                    {country}
+                                </li>
+                            ))}
+                            <hr />
+                            {countries
+                                .filter(
+                                    (country) => !fiCountries.includes(country)
+                                )
+                                .map((country) => (
+                                    <li
+                                        key={country}
+                                        onClick={() =>
+                                            handleCountryClickReset(country)
+                                        }
+                                        className="py-1 px-4 hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] cursor-pointer opacity-50"
+                                    >
+                                        {country}
+                                    </li>
+                                ))}
+                        </ul>
+                    )}
+                </div>
+
+                <div className="flex items-center ">
+                    <p>{films.length} Filmes</p>
+                </div>
+            </div>
+
+            <button
+                onClick={onToggleView}
+                className="border rounded-full px-3 py-1 items-center transition-all duration-300 ease-in-out"
+            >
+                {isListView ? "Galeria" : "Lista"}
+            </button>
+        </div>
+    );
 }
--- src/pages/Arquivo.jsx
+++ src/pages/Arquivo.jsx
@@ -1,129 +1,160 @@
-import { Link } from 'react-router-dom';
-import sanityClient from '../SanityClient.js';
-import React, { useEffect, useState } from 'react';
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
-import { FilterSearch } from '../components/ArquivoComponentes/FilterSearch.jsx';
-import { FilmItem } from '../components/ArquivoComponentes/FilmItem.jsx';
-import { createSlug } from '../utils/slug.js';
-import { ArchiveList } from '../components/ArquivoComponentes/ArchiveList.jsx';
-import { motion, AnimatePresence } from 'framer-motion';
+import { Link } from "react-router-dom";
+import sanityClient from "../SanityClient.js";
+import React, { useEffect, useState } from "react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+// import { useSearchParams } from "react-router-dom";
+import { FilterSearch } from "../components/ArquivoComponentes/FilterSearch.jsx";
+import { FilmItem } from "../components/ArquivoComponentes/FilmItem.jsx";
+import { createSlug } from "../utils/slug.js";
+import { ArchiveList } from "../components/ArquivoComponentes/ArchiveList.jsx";
+import { motion, AnimatePresence } from "framer-motion";
 
-const FILMS_PER_LOAD = 15;
+const search = ({ searchTerm, selectedYear, selectedCountry }) =>
+    sanityClient.fetch(`
+	*[
+		_type == "filme" 
+		&& !(_id in path("drafts.**"))
+		&& _score > 0
+		${selectedYear ? `&& ano == ${selectedYear}` : ""}
+		${selectedCountry ? `&& pais == "${selectedCountry}"` : ""}
+	] | score(
+		boost(nome match "*${searchTerm}*", 5),
+		realizador match "*${searchTerm}*",
+		autorEntrevista match "*${searchTerm}*",
+		sinopse match "*${searchTerm}*",
+		pt::text(entrevista) match "*${searchTerm}*",
+		creditos[].conteudo match "*${searchTerm}*"
+		) | order(_score desc) {
+			_score,
+			nome,
+			realizador,
+			ano,
+			pais,
+			minutos,
+			stills[0..4]
+		}`);
+
+const fetchAll = () =>
+    sanityClient.fetch(`
+	*[_type == "filme"] | order(_createdAt desc) {
+		nome,
+		realizador,
+		ano,
+		pais,
+		minutos,
+		stills[0..4] 
+	}
+`);
 
 export function Arquivo() {
-	const [allFilms, setAllFilms] = useState([]); 
-	const [filteredFilms, setFilteredFilms] = useState([]);
-	const [displayedFilms, setDisplayedFilms] = useState([]); 
-	const [isLoading, setIsLoading] = useState(true);
-	const [isListView, setIsListView] = useState(false);
-	const [showLoadMore, setShowLoadMore] = useState(true);
+    const [allFilms, setAllFilms] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isListView, setIsListView] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [selectedYear, setSelectedYear] = useState("");
+    const [selectedCountry, setSelectedCountry] = useState("");
+    // const [searchParams, setSearchParams] = useSearchParams();
 
-	useEffect(() => {
-		async function fetchFilms() {
-			const data = await sanityClient.fetch(`
-                *[_type == "filme"] | order(_createdAt desc) {
-                    nome,
-                    realizador,
-                    ano,
-					pais,
-                    minutos,
-                    stills[0..4] 
+    // useEffect(() => {
+    //     setSearchTerm(searchParams.get("search") || "");
+    // }, [searchParams.toString()]);
+
+    useEffect(() => {
+        if (
+            searchTerm.trim() === "" &&
+            selectedYear === "" &&
+            selectedCountry === ""
+        ) {
+            fetchAll().then((data) => {
+                setAllFilms(data);
+                setIsLoading(false);
+            });
+        } else {
+            search({ searchTerm, selectedYear, selectedCountry }).then(
+                (data) => {
+                    setAllFilms(data);
+                    setIsLoading(false);
                 }
-            `);
-			setAllFilms(data);
-			setFilteredFilms(data);
-			setDisplayedFilms(data.slice(0, FILMS_PER_LOAD)); 
-			setIsLoading(false);
-			setShowLoadMore(data.length > FILMS_PER_LOAD); 
-		}
+            );
+        }
+    }, [searchTerm, selectedYear, selectedCountry]);
 
-		fetchFilms();
-	}, []);
+    const onToggleView = () => {
+        setIsListView(!isListView);
+    };
 
-	const handleSearchChange = async ({ searchTerm, selectedYear, selectedCountry }) => {
-		const results = await sanityClient.fetch(`
-		*[
-			_type == "filme" 
-			&& !(_id in path("drafts.**"))
-			&& _score > 0
-			${selectedYear ? `&& ano == ${selectedYear}` : ''}
-			${selectedCountry ? `&& pais == "${selectedCountry}"` : ''}
-		] | score(
-			boost(nome match "*${searchTerm}*", 5),
-			realizador match "*${searchTerm}*",
-			autorEntrevista match "*${searchTerm}*",
-			sinopse match "*${searchTerm}*",
-			pt::text(entrevista) match "*${searchTerm}*",
-			creditos[].conteudo match "*${searchTerm}*"
-			) | order(_score desc) {
-				_score,
-				nome,
-				realizador,
-				ano,
-				pais,
-				minutos,
-				stills[0..4]
-			}`);
+    if (isLoading)
+        return (
+            <div className="w-screen h-screen fixed flex justify-center items-center">
+                A Carregar...
+            </div>
+        );
 
-		setFilteredFilms(results);
-		setDisplayedFilms(results); 
-		setShowLoadMore(false); 
-	};
+    return (
+        <>
+            <div className="margin-general pt-[2rem] ">
+                <FilterSearch
+                    films={allFilms}
+                    searchTerm={searchTerm}
+                    setSearchTerm={setSearchTerm}
+                    selectedYear={selectedYear}
+                    setSelectedYear={setSelectedYear}
+                    selectedCountry={selectedCountry}
+                    setSelectedCountry={setSelectedCountry}
+                    onToggleView={onToggleView}
+                    isListView={isListView}
+                />
 
-	const loadMoreFilms = () => {
-		const newDisplayCount = displayedFilms.length + FILMS_PER_LOAD;
-		setDisplayedFilms(filteredFilms.slice(0, newDisplayCount));
-		setShowLoadMore(newDisplayCount < filteredFilms.length); 
-	};
-
-	const onToggleView = () => {
-		setIsListView(!isListView);
-	};
-
-	if (isLoading) return <div className='w-screen h-screen fixed flex justify-center items-center'>A Carregar...</div>;
-
-	return (
-		<>
-			<div className='margin-general pt-[2rem] '>
-				<FilterSearch films={filteredFilms} onSearchChange={handleSearchChange} onToggleView={onToggleView} isListView={isListView} />
-
-				<div className='pt-8'>
-					<AnimatePresence mode='wait'>
-						{isListView ? (
-							<motion.div key='list' initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.3 }}>
-								<ArchiveList films={displayedFilms} />
-							</motion.div>
-						) : (
-							<motion.div key='gallery' initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.3 }}>
-								<ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }} className='w-full'>
-									<Masonry gutter='2rem'>
-										{displayedFilms.length > 0 ? (
-											displayedFilms.map(film => (
-												<Link to={`/Arquivo/${createSlug(film.nome)}`} key={film.nome}>
-													<FilmItem film={film} />
-												</Link>
-											))
-										) : (
-											<div>Filme não encontrado</div>
-										)}
-									</Masonry>
-								</ResponsiveMasonry>
-							</motion.div>
-						)}
-					</AnimatePresence>
-
-					{showLoadMore && (
-						<div className='flex justify-center mt-4'>
-							<button
-								onClick={loadMoreFilms}
-								className='border rounded-full py-1 px-3   hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] '
-							>
-								Carregar Mais
-							</button>
-						</div>
-					)}
-				</div>
-			</div>
-		</>
-	);
+                <div className="pt-8">
+                    <AnimatePresence mode="wait">
+                        {isListView ? (
+                            <motion.div
+                                key="list"
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.3 }}
+                            >
+                                <ArchiveList films={allFilms} />
+                            </motion.div>
+                        ) : (
+                            <motion.div
+                                key="gallery"
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.3 }}
+                            >
+                                <ResponsiveMasonry
+                                    columnsCountBreakPoints={{
+                                        350: 1,
+                                        750: 2,
+                                        900: 3,
+                                    }}
+                                    className="w-full"
+                                >
+                                    <Masonry gutter="2rem">
+                                        {allFilms.length > 0 ? (
+                                            allFilms.map((film) => (
+                                                <Link
+                                                    to={`/Arquivo/${createSlug(
+                                                        film.nome
+                                                    )}`}
+                                                    key={film.nome}
+                                                >
+                                                    <FilmItem film={film} />
+                                                </Link>
+                                            ))
+                                        ) : (
+                                            <div>Filme não encontrado</div>
+                                        )}
+                                    </Masonry>
+                                </ResponsiveMasonry>
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
+                </div>
+            </div>
+        </>
+    );
 }
