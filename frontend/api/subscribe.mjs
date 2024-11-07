52dcd2f0ce27ea9cac63abb77bf4dd1910da5ce2
Derek Nguyen <dikanguok@gmail.com>
2024-11-07 01:09:59 +0000

    fix: handle 40x

--- frontend/api/subscribe.mjs
+++ frontend/api/subscribe.mjs
@@ -1,60 +1,47 @@
 export async function POST(req) {
-	const { firstName, lastName, email } = await req.json();
-	const data = {
-		email_address: email,
-		status: 'subscribed',
-		merge_fields: { FNAME: firstName, LNAME: lastName },
-	};
-    console.log({
-			data,
-			url: `https://${process.env.MAILCHIMP_SERVER_PREFIX}.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_LIST_ID}/members`,
-    });
-    
-    try {
-        
-		 await fetch(`https://${process.env.MAILCHIMP_SERVER_PREFIX}.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_LIST_ID}/members`, {
-			method: 'POST',
-			body: JSON.stringify(data),
-			headers: {
-				Authorization: `apikey ${process.env.MAILCHIMP_API_KEY}`,
-			},
-		});
-		return  Response.json(
-			{ message: 'Inscrição realizada com sucesso!' },
-			{
-				status: 200,
-			},
-		); 
-	} catch (error) {
-		if (error.response) {
-			console.error('Error response from Mailchimp:', error.response.data);
+    const { firstName, lastName, email } = req.body;
+    const data = {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: { FNAME: firstName, LNAME: lastName },
+    };
 
-			if (error.response.data.title === 'Member Exists') {
-				return Response.json(
-					{ message: 'Este e-mail já se encontra na nossa lista.' },
-					{
-						status: 400,
-					},
-				);
-			} else {
-				return Response.json(
-					{
-						message: 'Erro ao se inscrever. Tente novamente mais tarde.',
-					},
-					{
-						status: 400,
-					},
-				);
-			}
-		} else {
-			return Response.json(
-				{
-					message: 'Erro ao se inscrever. Tente novamente mais tarde.',
-				},
-				{
-					status: 500,
-				},
-			);
-		}
-	}
+    const result = await fetch(
+        `https://${process.env.MAILCHIMP_SERVER_PREFIX}.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_LIST_ID}/members`,
+        {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                Authorization: `apikey ${process.env.MAILCHIMP_API_KEY}`,
+            },
+        }
+    );
+
+    if (result.status === 200) {
+        return Response.json(
+            { message: "Inscrição realizada com sucesso!" },
+            {
+                status: 200,
+            }
+        );
+    }
+
+    const { title } = await result.json();
+    if (title === "Member Exists") {
+        return Response.json(
+            { message: "Este e-mail já se encontra na nossa lista." },
+            {
+                status: 400,
+            }
+        );
+    } else {
+        return Response.json(
+            {
+                message: "Erro ao se inscrever. Tente novamente mais tarde.",
+            },
+            {
+                status: 400,
+            }
+        );
+    }
 }
