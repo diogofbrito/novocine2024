6f3bddd26a2d5180aeed3781b13453f4083721c5
Derek Nguyen <dikanguok@gmail.com>
2024-11-12 22:04:56 +0000

    fix(arquivo): cancel smooth scrolling when change page

--- frontend/src/components/ArquivoPagComp/ArchiveList.jsx
+++ frontend/src/components/ArquivoPagComp/ArchiveList.jsx
@@ -1,44 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useLang } from '../LangProvider';
-import { translation } from '../../Lang/translation.js';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useLang } from "../LangProvider";
+import { translation } from "../../Lang/translation.js";
 
-export function ArchiveList({ films }) {
-	const { lang } = useLang();
-	return (
-		<div className='w-full flex flex-col justify-center gap-4 '>
-			<div className='grid grid-cols-4  w-full px-4 '>
-				<p className='col-span-2 '>{translation[lang].titulo}</p>
-				<p className='col-span-1 '>{translation[lang].realizador}</p>
-				<div className='col-span-1 grid grid-cols-4 w-full'>
-					<p className='col-span-2'>{translation[lang].pais}</p>
-					<p className='col-span-1'>{translation[lang].duracao}</p>
-					<p className='col-span-1'>{translation[lang].ano}</p>
-				</div>
-			</div>
-			{films.length > 0 ? (
-				films.map(film => (
-					<Link
-						to={`/arquivo/${film.slug?.current}`}
-						key={film.slug?.current}
-						className='grid grid-cols-4 items-center w-full p-4 border rounded-3xl hover:bg-[var(--text-color-light)] dark:hover:bg-[var(--text-color-dark)]'
-					>
-						<p className='col-span-2 text-5xl font-cine'>{film.nome}</p>
+export function ArchiveList({ films, onFilmClick }) {
+    const { lang } = useLang();
+    return (
+        <div className="w-full flex flex-col justify-center gap-4 ">
+            <div className="grid grid-cols-4  w-full px-4 ">
+                <p className="col-span-2 ">{translation[lang].titulo}</p>
+                <p className="col-span-1 ">{translation[lang].realizador}</p>
+                <div className="col-span-1 grid grid-cols-4 w-full">
+                    <p className="col-span-2">{translation[lang].pais}</p>
+                    <p className="col-span-1">{translation[lang].duracao}</p>
+                    <p className="col-span-1">{translation[lang].ano}</p>
+                </div>
+            </div>
+            {films.length > 0 ? (
+                films.map((film) => (
+                    <Link
+                        onClick={onFilmClick}
+                        to={`/arquivo/${film.slug?.current}`}
+                        key={film.slug?.current}
+                        className="grid grid-cols-4 items-center w-full p-4 border rounded-3xl hover:bg-[var(--text-color-light)] dark:hover:bg-[var(--text-color-dark)]"
+                    >
+                        <p className="col-span-2 text-5xl font-cine">
+                            {film.nome}
+                        </p>
 
-						<p className='col-span-1 text-base'>
-							<span className='font-bold'>{film.realizador}</span>
-						</p>
+                        <p className="col-span-1 text-base">
+                            <span className="font-bold">{film.realizador}</span>
+                        </p>
 
-						<div className='col-span-1 grid grid-cols-4 w-full text-base'>
-							<p className='col-span-2'>{film.pais}</p>
-							<p className='col-span-1'>{film.minutos} min</p>
-							<p className='col-span-1'>{film.ano}</p>
-						</div>
-					</Link>
-				))
-			) : (
-				<div className='p-2'>No projects found</div>
-			)}
-		</div>
-	);
+                        <div className="col-span-1 grid grid-cols-4 w-full text-base">
+                            <p className="col-span-2">{film.pais}</p>
+                            <p className="col-span-1">{film.minutos} min</p>
+                            <p className="col-span-1">{film.ano}</p>
+                        </div>
+                    </Link>
+                ))
+            ) : (
+                <div className="p-2">No projects found</div>
+            )}
+        </div>
+    );
 }
--- frontend/src/pages/Arquivo.jsx
+++ frontend/src/pages/Arquivo.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import sanityClient from '../SanityClient.js';
-import Masonry from 'react-masonry-css';
-import { FilterSearch } from '../components/ArquivoPagComp/FilterSearch.jsx';
-import { FilmItem } from '../components/ArquivoPagComp/FilmItem.jsx';
-import { SkeletonArchiveMasonry } from '../components/Skeleton/SkeletonArchiveMasonry.jsx';
-import { ArchiveList } from '../components/ArquivoPagComp/ArchiveList.jsx';
-import { motion, AnimatePresence } from 'framer-motion';
-
+import React, { useEffect, useState, useCallback } from "react";
+import { Link } from "react-router-dom";
+import { useLenis } from "@studio-freight/react-lenis";
+import sanityClient from "../SanityClient.js";
+import Masonry from "react-masonry-css";
+import { FilterSearch } from "../components/ArquivoPagComp/FilterSearch.jsx";
+import { FilmItem } from "../components/ArquivoPagComp/FilmItem.jsx";
+import { SkeletonArchiveMasonry } from "../components/Skeleton/SkeletonArchiveMasonry.jsx";
+import { ArchiveList } from "../components/ArquivoPagComp/ArchiveList.jsx";
+import { motion, AnimatePresence } from "framer-motion";
 
 const search = ({ searchTerm, selectedYear, selectedCountry }) =>
-	sanityClient.fetch(`
+    sanityClient.fetch(`
 	*[
 		_type == "filme" 
 		&& !(_id in path("drafts.**"))
 		&& _score > 0
-		${selectedYear ? `&& ano == ${selectedYear}` : ''}
-		${selectedCountry ? `&& pais == "${selectedCountry}"` : ''}
+		${selectedYear ? `&& ano == ${selectedYear}` : ""}
+		${selectedCountry ? `&& pais == "${selectedCountry}"` : ""}
 	] | score(
 		boost(nome match "*${searchTerm}*", 5),
 		realizador match "*${searchTerm}*",
@@ -36,7 +36,7 @@ const search = ({ searchTerm, selectedYear, selectedCountry }) =>
 		}`);
 
 const fetchAll = () =>
-	sanityClient.fetch(`
+    sanityClient.fetch(`
 	*[_type == "filme"] | order(_createdAt desc) {
 		nome,
 		realizador,
@@ -49,78 +49,113 @@ const fetchAll = () =>
 `);
 
 export function Arquivo() {
-	const [allFilms, setAllFilms] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
-	const [isListView, setIsListView] = useState(false);
-	const [searchTerm, setSearchTerm] = useState('');
-	const [selectedYear, setSelectedYear] = useState('');
-	const [selectedCountry, setSelectedCountry] = useState('');
+    const [allFilms, setAllFilms] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isListView, setIsListView] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [selectedYear, setSelectedYear] = useState("");
+    const [selectedCountry, setSelectedCountry] = useState("");
 
-	useEffect(() => {
-		if (searchTerm.trim() === '' && selectedYear === '' && selectedCountry === '') {
-			fetchAll().then(data => {
-				setAllFilms(data);
-				setIsLoading(false);
-			});
-		} else {
-			search({ searchTerm, selectedYear, selectedCountry }).then(data => {
-				setAllFilms(data);
-				setIsLoading(false);
-			});
-		}
-	}, [searchTerm, selectedYear, selectedCountry]);
+    const lenis = useLenis();
 
-	const onToggleView = () => {
-		setIsListView(!isListView);
-	};
+    useEffect(() => {
+        if (
+            searchTerm.trim() === "" &&
+            selectedYear === "" &&
+            selectedCountry === ""
+        ) {
+            fetchAll().then((data) => {
+                setAllFilms(data);
+                setIsLoading(false);
+            });
+        } else {
+            search({ searchTerm, selectedYear, selectedCountry }).then(
+                (data) => {
+                    setAllFilms(data);
+                    setIsLoading(false);
+                }
+            );
+        }
+    }, [searchTerm, selectedYear, selectedCountry]);
 
-	const breakpointColumnsObj = {
-		default: 3,
-		1100: 2,
-		768: 1,
-	};
+    const onToggleView = () => {
+        setIsListView(!isListView);
+    };
 
-	return (
-		<>
-			<div className='mx-[4.5rem] mb-[3rem] mt-[4.5rem] pt-[2rem] iphone:pt-2 iphone:mx-[1.5rem] iphone:mb-[1.5rem] '>
-				<FilterSearch
-					films={allFilms}
-					searchTerm={searchTerm}
-					setSearchTerm={setSearchTerm}
-					selectedYear={selectedYear}
-					setSelectedYear={setSelectedYear}
-					selectedCountry={selectedCountry}
-					setSelectedCountry={setSelectedCountry}
-					onToggleView={onToggleView}
-					isListView={isListView}
-				/>
+    const breakpointColumnsObj = {
+        default: 3,
+        1100: 2,
+        768: 1,
+    };
 
-				<div className='pt-6'>
-					<AnimatePresence mode='wait'>
-						{isLoading ? (
-							<SkeletonArchiveMasonry />
-						) : (
-							<motion.div key={isListView ? 'list' : 'gallery'} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.3 }}>
-								{isListView ? (
-									<ArchiveList films={allFilms} />
-								) : (
-									<Masonry breakpointCols={breakpointColumnsObj} className='flex gap-6'>
-										{allFilms.length > 0 ? (
-											allFilms.map(film => (
-												<Link to={`/arquivo/${film.slug?.current}`} key={film.slug?.current} >
-													<FilmItem film={film} />
-												</Link>
-											))
-										) : (
-											<div>Filme não encontrado</div>
-										)}
-									</Masonry>
-								)}
-							</motion.div>
-						)}
-					</AnimatePresence>
-				</div>
-			</div>
-		</>
-	);
+    const cancelSmoothScrolling = useCallback(() => {
+        lenis.scrollTo(0, {
+            duration: 0,
+            lerp: 0,
+        });
+    }, [lenis]);
+
+    return (
+        <>
+            <div className="mx-[4.5rem] mb-[3rem] mt-[4.5rem] pt-[2rem] iphone:pt-2 iphone:mx-[1.5rem] iphone:mb-[1.5rem] ">
+                <FilterSearch
+                    films={allFilms}
+                    searchTerm={searchTerm}
+                    setSearchTerm={setSearchTerm}
+                    selectedYear={selectedYear}
+                    setSelectedYear={setSelectedYear}
+                    selectedCountry={selectedCountry}
+                    setSelectedCountry={setSelectedCountry}
+                    onToggleView={onToggleView}
+                    isListView={isListView}
+                />
+
+                <div className="pt-6">
+                    <AnimatePresence mode="wait">
+                        {isLoading ? (
+                            <SkeletonArchiveMasonry />
+                        ) : (
+                            <motion.div
+                                key={isListView ? "list" : "gallery"}
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.3 }}
+                            >
+                                {isListView ? (
+                                    <ArchiveList
+                                        films={allFilms}
+                                        onFilmClick={
+                                            cancelSmoothScrolling
+                                        }
+                                    />
+                                ) : (
+                                    <Masonry
+                                        breakpointCols={breakpointColumnsObj}
+                                        className="flex gap-6"
+                                    >
+                                        {allFilms.length > 0 ? (
+                                            allFilms.map((film) => (
+                                                <Link
+                                                    onClick={
+                                                        cancelSmoothScrolling
+                                                    }
+                                                    to={`/arquivo/${film.slug?.current}`}
+                                                    key={film.slug?.current}
+                                                >
+                                                    <FilmItem film={film} />
+                                                </Link>
+                                            ))
+                                        ) : (
+                                            <div>Filme não encontrado</div>
+                                        )}
+                                    </Masonry>
+                                )}
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
+                </div>
+            </div>
+        </>
+    );
 }
