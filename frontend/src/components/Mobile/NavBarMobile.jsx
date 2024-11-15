bc872203f72ab0d999d3f00107d99799627f0ba7
diogofbrito <diogobrito74@gmail.com>
2024-11-15 23:34:38 +0000

    feat: bold

--- frontend/src/components/Mobile/NavBarMobile.jsx
+++ frontend/src/components/Mobile/NavBarMobile.jsx
@@ -17,7 +17,7 @@ export function NavBarMobile({ translation, useLang, setShowNewsletter, close })
 	return (
 		<div className='hidden iphone:block relative justify-self-end'>
 			<button
-				className=' uppercase  p-2 border rounded-full hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] transition duration-300 ease-in-out font-bold '
+				className=' uppercase  p-2 border rounded-full hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] transition duration-300 ease-in-out '
 				onClick={toggleDrawer(true)}
 			>
 				<AlignJustify size={20} />
@@ -43,7 +43,7 @@ export function NavBarMobile({ translation, useLang, setShowNewsletter, close })
 				<div className='flex flex-col justify-between h-full py-6 px-2 '>
 					<div className='flex justify-end'>
 						<button
-							className='uppercase px-3 py-1 border rounded-full hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] transition duration-300 ease-in-out font-bold '
+							className='uppercase px-3 py-1 border rounded-full hover:bg-[rgba(255,255,255,0.5)] dark:hover:bg-[rgba(234,235,222,0.5)] transition duration-300 ease-in-out '
 							onClick={toggleDrawer(close)}
 						>
 							{translation[lang].fechar}
@@ -52,17 +52,17 @@ export function NavBarMobile({ translation, useLang, setShowNewsletter, close })
 
 					<div className='flex flex-col justify-start gap-4  '>
 						<div>
-							<NavLink to='/' onClick={toggleDrawer(false)} className='uppercase px-3 py-1 border rounded-full font-bold'>
+							<NavLink to='/' onClick={toggleDrawer(false)} className='uppercase px-3 py-1 border rounded-full '>
 								home
 							</NavLink>
 						</div>
 						<div>
-							<NavLink to='/arquivo' onClick={toggleDrawer(false)} className='uppercase px-3 py-1 border rounded-full font-bold'>
+							<NavLink to='/arquivo' onClick={toggleDrawer(false)} className='uppercase px-3 py-1 border rounded-full '>
 								{translation[lang].arquivo}
 							</NavLink>
 						</div>
 						<div>
-							<NavLink to='/sobre' onClick={toggleDrawer(false)} className='uppercase px-3 py-1 border rounded-full font-bold'>
+							<NavLink to='/sobre' onClick={toggleDrawer(false)} className='uppercase px-3 py-1 border rounded-full '>
 								{translation[lang].sobre}
 							</NavLink>
 						</div>
@@ -72,7 +72,7 @@ export function NavBarMobile({ translation, useLang, setShowNewsletter, close })
 									setShowNewsletter(true);
 									toggleDrawer(false)();
 								}}
-								className='uppercase px-3 py-1 border rounded-full font-bold'
+								className='uppercase px-3 py-1 border rounded-full '
 							>
 								NEWSLETTER
 							</button>
@@ -83,7 +83,7 @@ export function NavBarMobile({ translation, useLang, setShowNewsletter, close })
 						<button className='px-3 py-2 border rounded-full ' onClick={toggleTheme} aria-label='Toggle theme'>
 							{theme === 'light' ? <Moon size={19} /> : <Sun size={19} />}
 						</button>
-						<button onClick={toggleLang} className='px-3 py-1 border rounded-full font-bold'>
+						<button onClick={toggleLang} className='px-3 py-1 border rounded-full '>
 							{lang === 'PT' ? 'EN' : 'PT'}
 						</button>
 					</div>
