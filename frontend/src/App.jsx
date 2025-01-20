777df61fe90018156366ab768bf7fd65c55a33e6
diogofbrito <diogobrito74@gmail.com>
2025-01-20 20:50:09 +0000

    feat: analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Outlet} from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
 import { LangProvider } from './components/LangProvider';
 import { SmoothScrolling } from './components/SmoothScrolling';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
-
 	return (
-		<SmoothScrolling >
-		 <LangProvider>
-			<NavBar />
-			<Outlet />
-		</LangProvider>
+		<SmoothScrolling>
+			<LangProvider>
+				<Analytics />
+				<NavBar />
+				<Outlet />
+			</LangProvider>
 		</SmoothScrolling>
-		
 	);
 }
 
